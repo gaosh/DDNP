80cf3db7b368f30ca64c24a91548c0f6cf7130d9
gaosh <gao.shanqian@gmail.com>
2022-07-14 20:06:55 -0400

    Update bash_resnet.sh

--- bash_resnet.sh
+++ bash_resnet.sh
@@ -1,7 +1,7 @@
 #train the base model
 CUDA_VISIBLE_DEVICES=0 nohup python train_model.py --smooth_flag False --train_base True --lr 0.1 > base_rn56.txt 2>&1 &
 #To train the pruning algorithm
-CUDA_VISIBLE_DEVICES=0 python resnet_topk.py --reg_w 2 --base 3.0
+CUDA_VISIBLE_DEVICES=0 nohup python resnet_topk.py --reg_w 2 --base 3.0 > prune_rn56.txt 2>&1 &
 #prune the model
 python pruning_resnet.py
 #finetune the model
